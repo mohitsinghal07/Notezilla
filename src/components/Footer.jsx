6dbf83c4503d167521aaef6e3383693d8924ec4d
Mohit Singhal <mohitsinghal321@gmail.com>
2022-08-18 00:04:03 +0530

    Notezilla - note taking app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright ⓒ Notezilla {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
